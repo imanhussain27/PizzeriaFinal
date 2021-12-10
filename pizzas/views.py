9c59cad37c6e4e2a77b5508539ff1f0f703ed501
imanhussain27 <iman_hussain1@baylor.edu>
2021-12-09 22:49:00 -0600

    several formatting changes

--- pizzas/views.py
+++ pizzas/views.py
@@ -21,7 +21,7 @@ def pizza(request, pizza_id):
     toppings = pizza.toppings_set.order_by('-date_added')
     comments = pizza.comment_set.order_by('-date_added')
 
-    context = {'pizza': pizza, 'toppings': toppings}
+    context = {'pizza': pizza, 'toppings': toppings, 'comments': comments}
     return render(request, 'pizzas/pizza.html', context)
 
 @login_required
